bc7080b25444f1d15efec21f9f86d3d8d7f010bd
Ahmed Azeem <45334554+amdazm05@users.noreply.github.com>
2023-05-13 17:33:38 +0500

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,7 +5,7 @@ int main()
     char buffer[100];
     UDP_Non_Blocking client;
     client.initialise(8080);
-    client.UDPPacketRecieverFromDeviceToBuffer((unsigned char *)buffer,sizeof(buffer));
+    client.UDPPacketReciever((unsigned char *)buffer,sizeof(buffer));
 
     return 0;
 }
